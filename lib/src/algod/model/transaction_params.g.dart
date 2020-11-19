fda02f8c55c064fd0b367afd75597957cbb4a6ac
Gianluca Mazza <info@gianlucamazza.it>
2020-11-19 17:03:09 +0100

    fix

--- lib/src/algod/model/transaction_params.g.dart
+++ lib/src/algod/model/transaction_params.g.dart
@@ -7,7 +7,7 @@ part of 'transaction_params.dart';
 // **************************************************************************
 
 Serializer<TransactionParams> _$transactionParamsSerializer =
-    _$TransactionParamsSerializer();
+    new _$TransactionParamsSerializer();
 
 class _$TransactionParamsSerializer
     implements StructuredSerializer<TransactionParams> {
@@ -63,7 +63,7 @@ class _$TransactionParamsSerializer
   TransactionParams deserialize(
       Serializers serializers, Iterable<Object> serialized,
       {FullType specifiedType = FullType.unspecified}) {
-    final result = TransactionParamsBuilder();
+    final result = new TransactionParamsBuilder();
 
     final iterator = serialized.iterator;
     while (iterator.moveNext()) {
@@ -118,7 +118,7 @@ class _$TransactionParams extends TransactionParams {
 
   factory _$TransactionParams(
           [void Function(TransactionParamsBuilder) updates]) =>
-      (TransactionParamsBuilder()..update(updates)).build();
+      (new TransactionParamsBuilder()..update(updates)).build();
 
   _$TransactionParams._(
       {this.consensusVersion,
@@ -135,7 +135,7 @@ class _$TransactionParams extends TransactionParams {
 
   @override
   TransactionParamsBuilder toBuilder() =>
-      TransactionParamsBuilder()..replace(this);
+      new TransactionParamsBuilder()..replace(this);
 
   @override
   bool operator ==(Object other) {
@@ -222,7 +222,7 @@ class TransactionParamsBuilder
   @override
   void replace(TransactionParams other) {
     if (other == null) {
-      throw ArgumentError.notNull('other');
+      throw new ArgumentError.notNull('other');
     }
     _$v = other as _$TransactionParams;
   }
@@ -235,7 +235,7 @@ class TransactionParamsBuilder
   @override
   _$TransactionParams build() {
     final _$result = _$v ??
-        _$TransactionParams._(
+        new _$TransactionParams._(
             consensusVersion: consensusVersion,
             fee: fee,
             genesisID: genesisID,
